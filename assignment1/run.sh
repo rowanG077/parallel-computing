508af3ed6f049f88e4d740e268adf1d9c78ecefd
Gidoverheijen <104766980+Gidoverheijen@users.noreply.github.com>
2022-05-06 10:18:51 +0200

    Add files via upload

---
+++ assignment1/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --partition=csedu
+#SBATCH --output=output/std_%A_%a.txt
+#SBATCH --error=output/%A_%a.err
+#SBATCH --mem=100M
+#SBATCH --time=4-0:00:00
+#SBATCH --array=0-8%1
+
+matrix_sizes=($(seq 1 1 8))
+repetitions=($(seq 1 1 3))
+cores=($(seq 1 1 10))
+m_size=${matrix_sizes[$SLURM_ARRAY_TASK_ID]}
+rep=${repetitions[$SLURM_ARRAY_TASK_ID]}
+num_cores=${cores[$SLURM_ARRAY_TASK_ID]}
+cd ~/Parallel_computing
+mpicc -fopenmp -DMPI -o relax relax_omp.c
+time salloc -p csedu --cpus-per-task=${num_cores} -n 1 mpirun ./relax $[10**${m_size}] 500 4
